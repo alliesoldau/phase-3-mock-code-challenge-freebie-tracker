cc4221f281e4a4b436641a63e4a1da6dc77be071
Allie Soldau <alliesoldau@gmail.com>
2022-12-21 10:42:04 -0500

    complete

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.find_by(item_name: item_name) !=nil ? true : false
+    end
+
+    def give_away(dev, freebie)
+        self.id == freebie.dev.id ? freebie.update(dev_id: dev.id) : "You can't give away something that isn't yours"
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,24 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
+freebie_array=["Socks", "Mug", "Bobble Head", "Pen", "Frisbee", "Mouse Pad", "T-Shirt"]
+
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+30.times do
+    random_dev = rand(1..4)
+    random_company = rand(1..4)
+    random_freebie = rand(0..6)
+    Freebie.create(
+        item_name: freebie_array[random_freebie],
+        value: rand(1..100),
+        dev_id: random_dev,
+        company_id: random_company
+    )
+end
 
 puts "Seeding done!"
